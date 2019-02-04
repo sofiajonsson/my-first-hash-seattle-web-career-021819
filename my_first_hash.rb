1694666d80e0844a376e8831bfde4e1efc16b534
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 04:39:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  this_coder => {"name"}
+  this_coder => {"first_name" => "Sofia"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
@@ -10,7 +10,7 @@ def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+the_manifest = {"" =>""}
 
 end
 

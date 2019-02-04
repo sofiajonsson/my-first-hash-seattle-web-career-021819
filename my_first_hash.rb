81b7f8c2a7b1ec5e6fb78fa28d864909efaa0dc2
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-04 04:46:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,5 @@
 def my_hash
-  this_coder => {"first name" => "Sofia"}
+  this_coder = {"first name" => "Sofia"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
